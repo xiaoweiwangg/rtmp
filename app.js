51bc8553f76a85e1e5f56fc357a024678d980fd6
wangerwei <172345562@qq.com>
2019-10-07 20:19:51 +0800

    resetvideo

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ app.use("/",express.static("./"))
 var cmd=require('node-cmd');
   
     cmd.get(
-        'ffmpeg -i ./mv.mp4 -c copy -map 0 -f segment -segment_list ./v_map/mv.m3u8 -segment_time 5  ./v_map/mv-%03d.ts',//画图板
+        'ffmpeg -i ./02.mp4 -c copy -map 0 -f segment -segment_list ./v_map/02.m3u8 -segment_time 5  ./v_map/02-%03d.ts',//画图板
     function(data){
         console.log("执行完成！")
         }
